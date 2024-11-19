086470ad753f1ae39a27989717d9e93f51a13750
Hyunmin Park <753429@student.dusd.net>
2024-11-19 15:30:02 -0800

    Challeneg 14

--- CodingChallengesParkP7/Program.cs
+++ CodingChallengesParkP7/Program.cs
@@ -31,7 +31,8 @@ class Challenges
         Console.WriteLine("Welcome to my coding challenge program. I will be making a variety of functions for you to use. \nPlease pick one.");
         Console.WriteLine("\n1. Add two numbers \n2. Convert minuttes to seconds \n3. Add one to a number \n4. Find the circuit power \n5. Convert years to days \n6. Find the area of a triangle " +
             "\n7. Number less than or greater than Zero \n8. Check if the sum of two numbers is greater tthan or less than 100 \n9. Check if two numbers are equal " +
-            "\n10. Add the phrase 'something' to an argument \n11. Give the opposite of true or false. \n12. Convert hours into seconds. \n13. Take a polygon and return its sum of internal degrees.");
+            "\n10. Add the phrase 'something' to an argument \n11. Give the opposite of true or false. \n12. Convert hours into seconds. \n13. Take a polygon and return its sum of internal degrees." +
+            "\n14. Take a string and add Edabit");
         
         var funcSelect = Console.ReadLine();
         Console.Clear();
@@ -233,6 +234,14 @@ class Challenges
             }
 
 
+
+        }
+        else if(funcSelect == "14")
+        {
+            Console.WriteLine("Today we will be taking a sting and adding the word Edabit to it. Please give me a string");
+                string passString = Console.ReadLine();
+            var result = passString + "Edabit";
+            Console.WriteLine(result);
         }
         else
         {
